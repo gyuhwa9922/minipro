13d685b3f4ee45167b51f3be37d00e30173d48c8
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2022-11-29 19:19:46 +0900

    Add files via upload

--- front/pages/signup.jsx
+++ front/pages/signup.jsx
@@ -5,23 +5,23 @@ import useinput from "../hooks/useinput";
 
 const signup = () => {
   // 아이디
-  // const [id, onChangeId] = useinput("");
-  const [id, setId] = useState("");
-  const onChangeId = useCallback((e) => {
-    setId(e.target.value);
-  }, []);
+  const [id, onChangeId] = useinput("");
+  // const [id, setId] = useState("");
+  // const onChangeId = useCallback((e) => {
+  //   setId(e.target.value);
+  // }, []);
   // 비밀번호
-  // const [password, onChangePassword] = useinput("");
-  const [password, setpassword] = useState("");
-  const onChangePassword = useCallback((e) => {
-    setpassword(e.target.value);
-  }, []);
+  const [password, onChangePassword] = useinput("");
+  // const [password, setpassword] = useState("");
+  // const onChangePassword = useCallback((e) => {
+  //   setpassword(e.target.value);
+  // }, []);
   // 닉네임
-  // const [Nickname, onChangeNickname] = useinput("");
-  const [Nickname, setNickname] = useState("");
-  const onChangeNickname = useCallback((e) => {
-    setNickname(e.target.value);
-  }, []);
+  const [Nickname, onChangeNickname] = useinput("");
+  // const [Nickname, setNickname] = useState("");
+  // const onChangeNickname = useCallback((e) => {
+  //   setNickname(e.target.value);
+  // }, []);
   // 비밀번호 에러 && 비밀번호 체크
   const [passwordError, setpasswordError] = useState(false);
   const [passwordCheck, setpasswordCheck] = useState("");
@@ -45,7 +45,7 @@ const signup = () => {
       // console.log("flag1");
       return setpasswordError(true);
     }
-    if (!termError) {
+    if (!term) {
       // console.log("flag2");
       // console.log(termError);
       return settermError(true);
@@ -55,12 +55,8 @@ const signup = () => {
     console.log(password);
     console.log(term);
     console.log("flag3");
-  }, []);
+  }, [password, passwordCheck, term]);
 
-  console.log(id);
-  console.log(Nickname);
-  console.log(password);
-  console.log(term);
   return (
     <AppLayout>
       <Form onFinish={onSubmit}>
