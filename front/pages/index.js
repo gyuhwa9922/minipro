196e71e1ceca446e8820d49b1d81726d102c938b
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2022-12-05 17:27:34 +0900

    Add files via upload

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,6 +1,18 @@
+import { useSelector } from "react-redux";
 import AppLayout from "../components/AppLayout";
-
-const home = () => {
-  return <AppLayout />;
+import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+import user from "../reducers/user";
+const Home = () => {
+  const { isLoggedIn } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
+  return (
+    <AppLayout>
+      {isLoggedIn && <PostForm />}
+      {mainPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </AppLayout>
+  );
 };
-export default home;
+export default Home;
