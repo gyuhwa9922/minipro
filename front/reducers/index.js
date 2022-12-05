a461d0feb3900479642927bfdc14f8f45d346e82
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2022-12-05 17:27:58 +0900

    Add files via upload

--- front/reducers/index.js
+++ front/reducers/index.js
@@ -1,43 +1,22 @@
 import { HYDRATE } from "next-redux-wrapper";
 import user from "./user";
 import post from "./post";
+import { combineReducers } from "redux";
 
-const initialState = {
-  user: {},
-  post: {},
-};
-export const loginAction = (data) => {
-  return {
-    type: "LOG_IN",
-    data,
-  };
-};
-export const logoutAction = () => {
-  return {
-    type: "LOG_OUT",
-  };
-};
-const ChangeNickname = (data) => {
-  return {
-    type: "CHAGE_NICKNAME",
-    data,
-  };
-};
 // (이전상태 , 액션 ) = > 다음상태
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case HYDRATE:
-      console.log("HYDRATE", action);
-      return { ...state, ...action.payload };
-    case "CHAGE_NICKNAME":
-      return {
-        ...state,
-        name: action.data,
-      };
-
-    default:
-      return state;
-  }
-};
+// 리덕스 리듀서를 작성할 때 가장 일반적인 용례를 단순화 하는 유틸리티 함수입니다.
+const rootReducer = combineReducers({
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        console.log("HYDRATE", action);
+        return { ...state, ...action.payload };
+      default:
+        return state;
+    }
+  },
+  user,
+  post,
+});
 
 export default rootReducer;
--- front/reducers/post.jsx
+++ front/reducers/post.jsx
@@ -1,10 +1,67 @@
 //게시물
 export const initialState = {
-  mainPosts: [],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "이규화",
+      },
+      content: "test1",
+      Images: [
+        {
+          src: "https://mblogthumb-phinf.pstatic.net/MjAyMDA0MDhfMTY2/MDAxNTg2MzI4MjMxMDU1.H7MqVj5K4vIoTrpk3zQ4zGKqFrg56RJyoVKHl0WeN2Ag.W3q_Cj7snD_j1MyIGmirukBFTzy3jGWrBZYHFxAVvOsg.GIF.superstarmark/%EC%82%AC%EC%A7%84%ED%8E%B8%EC%A7%91.gif?type=w2",
+        },
+        {
+          src: "https://mblogthumb-phinf.pstatic.net/MjAyMDA0MDhfMTY2/MDAxNTg2MzI4MjMxMDU1.H7MqVj5K4vIoTrpk3zQ4zGKqFrg56RJyoVKHl0WeN2Ag.W3q_Cj7snD_j1MyIGmirukBFTzy3jGWrBZYHFxAVvOsg.GIF.superstarmark/%EC%82%AC%EC%A7%84%ED%8E%B8%EC%A7%91.gif?type=w2",
+        },
+        {
+          src: "https://mblogthumb-phinf.pstatic.net/MjAyMDA0MDhfMTY2/MDAxNTg2MzI4MjMxMDU1.H7MqVj5K4vIoTrpk3zQ4zGKqFrg56RJyoVKHl0WeN2Ag.W3q_Cj7snD_j1MyIGmirukBFTzy3jGWrBZYHFxAVvOsg.GIF.superstarmark/%EC%82%AC%EC%A7%84%ED%8E%B8%EC%A7%91.gif?type=w2",
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: "babo",
+          },
+          content: "testcontent1",
+        },
+        {
+          User: {
+            nickname: "bab",
+          },
+          content: "testcontent2",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = "ADD_POST";
+export const addPost = {
+  type: ADD_POST,
+};
+const dummyPost = {
+  id: 1,
+  content: "dummy",
+  User: {
+    id: 1,
+    nickname: "lgh",
+  },
+  Images: [],
+  Comments: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
     default:
       return state;
   }
