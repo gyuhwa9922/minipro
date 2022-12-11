7c8c940a14580433b51edaaad42b87c1ee0586dd
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2022-12-11 23:50:54 +0900

    Add files via upload

--- front/store/configureStore.jsx
+++ front/store/configureStore.jsx
@@ -2,9 +2,10 @@ import { createWrapper } from "next-redux-wrapper";
 import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "../reducers";
 import { composeWithDevTools } from "@redux-devtools/extension";
+import thunkMiddleware from "redux-thunk";
 
 const configureStore = () => {
-  const middlewares = [];
+  const middlewares = [thunkMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
