f85f29454abff8132193fa9b551fa29ca94df4c5
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2022-12-27 18:11:06 +0900

    Add files via upload

--- front/reducers/user.jsx
+++ front/reducers/user.jsx
@@ -1,6 +1,8 @@
 // 유저
 export const initialState = {
-  inLoggedIn: false,
+  inLoggedIn: false, //로그인이 되어있나?
+  isLoggingIn: false, // 로그인 시도
+  isLoggingOut: false, // 로그아웃 시도
   user: null,
   signUpData: {},
   loginData: {},
@@ -13,32 +15,34 @@ export const loginAction = (data) => {
     axios
       .post("@@@/@@@")
       .then((res) => {
-        loginSuccessAction(res.data);
+        dispatch(loginSuccessAction(res.data));
       })
       .catch((err) => {
-        loginFailedAction(err);
+        dispatch(loginFailedAction(err));
       });
   };
 };
 
 export const loginRequestAction = (data) => {
+  console.log("로그인요청");
   return {
     type: "LOG_IN_REQUEST",
     data,
   };
 };
-export const loginSuccessAction = (data) => {
-  return {
-    type: "LOG_IN_SUCCESS",
-    data,
-  };
-};
-export const loginFailedAction = (data) => {
-  return {
-    type: "LOG_IN_FAILED",
-    data,
-  };
-};
+
+// export const loginSuccessAction = (data) => {
+//   return {
+//     type: "LOG_IN_SUCCESS",
+//     data,
+//   };
+// };
+// export const loginFailedAction = (data) => {
+//   return {
+//     type: "LOG_IN_FAILED",
+//     data,
+//   };
+// };
 export const logoutRequestAction = () => {
   return {
     type: "LOG_OUT_REQUEST",
@@ -57,17 +61,41 @@ export const logoutFailedAction = () => {
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "LOG_IN":
+    case "LOG_IN_REQUEST":
+      console.log("flag1");
       return {
         ...state,
-        isLoggedIn: true,
-        user: action.data,
+        isLoggingIn: true,
       };
-    case "LOG_OUT":
+    case "LOG_IN_SUCCESS":
       return {
         ...state,
+        isLoggingIn: false,
+        isLoggedIn: true,
+        user: { ...action.data, nickname: "lgh" },
+      };
+    case "LOG_IN_FAILED":
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: false,
+      };
+    case "LOG_OUT_REQUEST":
+      return {
+        ...state,
+        isLoggingOut: true,
+      };
+    case "LOG_OUT_SUCCESS":
+      return {
+        ...state,
+        isLoggingOut: false,
         isLoggedIn: false,
         user: null,
+      };
+    case "LOG_OUT_FAILED":
+      return {
+        ...state,
+        isLoggingOut: false,
       };
     default:
       return state;
