0b35dac27ef5fbca44301c4eed1cf108ff9fd5bc
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2022-12-29 19:26:36 +0900

    Add files via upload

--- front/reducers/post.jsx
+++ front/reducers/post.jsx
@@ -39,7 +39,10 @@ export const initialState = {
   postAdded: false,
 };
 
-const ADD_POST = "ADD_POST";
+export const ADD_POST_REQUEST = "ADD_POST_REQUEST";
+export const ADD_POST_SUCCESS = "ADD_POST_SUCCESS";
+export const ADD_POST_FAILED = "ADD_POST_FAILED";
+
 export const addPost = {
   type: ADD_POST,
 };
--- front/reducers/user.jsx
+++ front/reducers/user.jsx
@@ -7,6 +7,42 @@ export const initialState = {
   signUpData: {},
   loginData: {},
 };
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST";
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
+export const LOG_IN_FAILED = "LOG_IN_FAILED";
+
+export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+export const LOG_OUT_FAILED = "LOG_OUT_FAILED";
+
+export const SIGN_OUT_REQUEST = "SIGN_OUT_REQUEST";
+export const SIGN_OUT_SUCCESS = "SIGN_OUT_SUCCESS";
+export const SIGN_OUT_FAILED = "SIGN_OUT_FAILED";
+
+export const CHANGE_NICKNAME_REQUEST = "CHANGE_NICKNAME_REQUEST";
+export const CHANGE_NICKNAME_SUCCESS = "CHANGE_NICKNAME_SUCCESS";
+export const CHANGE_NICKNAME_FAILURE = "CHANGE_NICKNAME_FAILURE";
+
+export const FOLLOW_REQUEST = "FOLLOW_REQUEST";
+export const FOLLOW_SUCCESS = "FOLLOW_SUCCESS";
+export const FOLLOW_FAILURE = "FOLLOW_FAILURE";
+
+export const UNFOLLOW_REQUEST = "UNFOLLOW_REQUEST";
+export const UNFOLLOW_SUCCESS = "UNFOLLOW_SUCCESS";
+export const UNFOLLOW_FAILURE = "UNFOLLOW_FAILURE";
+
+export const ADD_POST_TO_ME = "ADD_POST_TO_ME";
+export const REMOVE_POST_OF_ME = "REMOVE_POST_OF_ME";
+
+// export const
+// export const
+// export const
+// export const
+// export const
+// export const
+// export const
+// export const
+// export const
 
 export const loginAction = (data) => {
   return (dispatch, getState) => {
@@ -26,73 +62,64 @@ export const loginAction = (data) => {
 export const loginRequestAction = (data) => {
   console.log("로그인요청");
   return {
-    type: "LOG_IN_REQUEST",
+    type: LOG_IN_REQUEST,
     data,
   };
 };
 
-// export const loginSuccessAction = (data) => {
-//   return {
-//     type: "LOG_IN_SUCCESS",
-//     data,
-//   };
-// };
-// export const loginFailedAction = (data) => {
-//   return {
-//     type: "LOG_IN_FAILED",
-//     data,
-//   };
-// };
 export const logoutRequestAction = () => {
+  console.log("로그아웃 요청");
   return {
-    type: "LOG_OUT_REQUEST",
+    type: LOG_OUT_REQUEST,
   };
 };
 
 export const logoutSuccessAction = () => {
+  console.log("로그인 성공");
   return {
-    type: "LOG_OUT_SUCCESS",
+    type: LOG_OUT_SUCCESS,
   };
 };
 export const logoutFailedAction = () => {
+  console.log("로그인 실패");
   return {
-    type: "LOG_OUT_FAILED",
+    type: LOG_OUT_FAILED,
   };
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "LOG_IN_REQUEST":
+    case LOG_IN_REQUEST:
       console.log("flag1");
       return {
         ...state,
         isLoggingIn: true,
       };
-    case "LOG_IN_SUCCESS":
+    case LOG_IN_SUCCESS:
       return {
         ...state,
         isLoggingIn: false,
         isLoggedIn: true,
         user: { ...action.data, nickname: "lgh" },
       };
-    case "LOG_IN_FAILED":
+    case LOG_IN_FAILED:
       return {
         ...state,
         isLoggingIn: false,
         isLoggedIn: false,
       };
-    case "LOG_OUT_REQUEST":
+    case LOG_OUT_REQUEST:
       return {
         ...state,
         isLoggingOut: true,
       };
-    case "LOG_OUT_SUCCESS":
+    case LOG_OUT_SUCCESS:
       return {
         ...state,
         isLoggingOut: false,
         isLoggedIn: false,
         user: null,
       };
-    case "LOG_OUT_FAILED":
+    case LOG_OUT_FAILED:
       return {
         ...state,
         isLoggingOut: false,
