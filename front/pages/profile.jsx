abe522c67d18ebe03e4f9bfb42aa151bf267b027
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2023-01-29 21:35:59 +0900

    Add files via upload

--- front/pages/profile.jsx
+++ front/pages/profile.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import AppLayout from "../components/AppLayout";
 import FollowList from "../components/FollowList";
 import NicknameEditForm from "../components/NicknameEditForm";
+import Router from "next/router";
 
-const profile = () => {
+ const profile = () => {
   const { user } = useSelector((state) => state.user);
 
+  useEffect(() => {
+    if(!(user $$ user.id)) {
+      Router.push('/');
+    }
+  });
+
+  if (!user) {
+    return null;
+  }
   // const followerList = [
   //   { nickname: "gyuhwa" },
   //   { nickname: "lgh" },
