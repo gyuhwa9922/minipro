b47fccfe45aeaf86a4af5aa7d3990cfb670c3101
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2023-02-10 23:59:12 +0900

    Add files via upload

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,44 +1,44 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AppLayout from "../components/AppLayout";
 import PostCard from "../components/PostCard";
 import PostForm from "../components/PostForm";
 import { LOAD_POST_REQUEST } from "../reducers/post";
-import user from "../reducers/user";
 
 const Home = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
-  const { mainPosts, hasMorePost } = useSelector((state) => state.post);
+  const { mainPosts, hasMorePost, loadPostLoading } = useSelector(
+    (state) => state.post
+  );
+  // console.log(mainPosts);
 
   useEffect(() => {
     dispatch({
       type: LOAD_POST_REQUEST,
     });
   }, []);
+
   useEffect(() => {
     function onScroll() {
-      console.log(
-        window.scrollY,
-        document.documentElement.clientHeight,
-        document.documentElement.scrollHeight
-      );
-    }
-    if (
-      window.scrollY + document.documentElement.clientHeight >
-      document.documentElement.scrollHeight - 300
-    ) {
-      if (hasMorePost) {
-        dispatch({
-          type: LOAD_POST_REQUEST,
-        });
+      if (
+        window.scrollY + document.documentElement.clientHeight ===
+        document.documentElement.scrollHeight - 300
+      ) {
+        console.log("게시물 로드중");
+        if (hasMorePost) {
+          dispatch({
+            type: LOAD_POST_REQUEST,
+          });
+        }
       }
     }
-    window.addEventListener("scroll", onscroll);
+    window.addEventListener("scroll", onScroll);
     return () => {
       window.removeEventListener("scroll", onScroll);
     };
-  }, [hasMorePost]);
+  }, [mainPosts, hasMorePost, loadPostLoading]);
+
   return (
     <AppLayout>
       {user && <PostForm />}
